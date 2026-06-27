cdb0f6a4f395fb8ffcc9c3baa80ff53ddbe7536e
ballasrr <146126538+ballasrr@users.noreply.github.com>
2026-06-28 02:53:28 +0300

    feat: add models, migrations, alembic config

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hello, World!")
+from fastapi import FastAPI
+
+app = FastAPI(title="E13-WASH API", version="1.0.0")
+
+
+@app.get("/")
+async def root():
+    return {"status": "ok", "project": "E13-WASH", "version": "1.0.0"}
